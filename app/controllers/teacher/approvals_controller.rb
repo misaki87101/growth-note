000805f9e1168c92f61869b19da6e80067016e6e
misaki87101 <misaki.0427z@gmail.com>
2026-03-26 17:53:21 +0900

    fix2_グループ間でのセキ ュリティ分離

--- app/controllers/teacher/approvals_controller.rb
+++ app/controllers/teacher/approvals_controller.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 # app/controllers/teacher/approvals_controller.rb (一例)
-class Teacher::ApprovalsController < ApplicationController
-def index
-  # 自分が担当しているクラスの、承認待ち生徒だけを取得
-  @pending_users = GroupUser.where(group: current_user.groups, accepted: false).includes(:user, :group)
-end
+module Teacher
+  class ApprovalsController < ApplicationController
+    def index
+      # 自分が担当しているクラスの、承認待ち生徒だけを取得
+      @pending_users = GroupUser.where(group: current_user.groups, accepted: false).includes(:user, :group)
+    end
 
-def update
-  @group_user = GroupUser.find(params[:id])
-  @group_user.update(accepted: true) # ここで承認！
-  redirect_to teacher_approvals_path, notice: "#{@group_user.user.name}さんを承認しました！"
+    def update
+      @group_user = GroupUser.find(params[:id])
+      @group_user.update(accepted: true) # ここで承認！
+      redirect_to teacher_approvals_path, notice: "#{@group_user.user.name}さんを承認しました！"
+    end
+  end
 end
