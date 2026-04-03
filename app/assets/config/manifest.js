1604b1a10eadc625acc95c4faa4aa5aad92f503a
misaki87101 <misaki.0427z@gmail.com>
2026-04-04 03:15:29 +0900

    フィードバックの写真投稿_一旦コメントアウトする6

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,2 @@
 //= link_tree ../images
 //= link_tree ../builds
-
-//= link_tree ../../javascript .js
-//= link application.css
-
-//= link_tree ../../javascript/controllers .js
-//= link_tree ../../../vendor/javascript .js
