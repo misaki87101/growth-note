1bbd917a37c04018d2e54535e75bd6b44df13920
misaki87101 <misaki.0427z@gmail.com>
2026-04-17 12:11:00 +0900

    本番環境のメール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,23 @@ Rails.application.configure do
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
-  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_caching = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'growth-note.onrender.com' }
+
+  config.action_mailer.smtp_settings = {
+  address:              'smtp-relay.brevo.com',
+  port:                 587,
+  domain:               'growth-note.onrender.com',
+  user_name:            'a861f9001@smtp-brevo.com', # SMTP画面にあるLogin
+  password:             ENV['BREVO_SMTP_KEY'], # Render側で環境変数に設定します
+  authentication:       'plain',
+  enable_starttls_auto: true
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
